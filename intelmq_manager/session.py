ce4067a382d02e67be2c9f5fcd3546033c8273d9
Bernhard Herzog <bernhard.herzog@intevation.de>
2020-07-14 18:57:05 +0200

    Add type annotations to intelmq_manager.session

--- intelmq_manager/session.py
+++ intelmq_manager/session.py
@@ -10,7 +10,7 @@ Author(s):
 
 import os
 import string
-import typing
+from typing import Tuple, Optional, Union
 from contextlib import contextmanager
 import json
 import threading
@@ -70,7 +70,7 @@ class SessionStore:
     """Session store based on SQLite
     """
 
-    def __init__(self, dbname, max_duration):
+    def __init__(self, dbname: str, max_duration: int):
         self.dbname = dbname
         self.max_duration = max_duration
         if not os.path.isfile(self.dbname):
@@ -78,7 +78,7 @@ class SessionStore:
         self.lock = threading.Lock()
         self.connection = self.connect()
 
-    def connect(self):
+    def connect(self) -> sqlite3.Connection:
         return sqlite3.connect(self.dbname, check_same_thread=False,
                                isolation_level=None)
 
@@ -91,7 +91,7 @@ class SessionStore:
         with self.connect() as con:
             con.executescript(INIT_DB_SQL)
 
-    def execute(self, stmt, params):
+    def execute(self, stmt: str, params: tuple) -> Optional[tuple]:
         with self.get_con() as con:
             return con.execute(stmt, params).fetchone()
 
@@ -103,23 +103,23 @@ class SessionStore:
     def expire_sessions(self):
         self.execute(EXPIRATION_SQL, (self.max_duration,))
 
-    def get(self, session_id):
+    def get(self, session_id: str) -> Optional[dict]:
         self.expire_sessions()
         row = self.execute(LOOKUP_SESSION_SQL, (session_id,))
         if row is not None:
             return json.loads(row[0])
         return None
 
-    def set(self, session_id, session_data):
+    def set(self, session_id: str, session_data: dict):
         self.execute(STORE_SESSION_SQL,
                      (session_id, json.dumps(session_data)))
 
-    def new_session(self, session_data):
+    def new_session(self, session_data: dict) -> str:
         token = os.urandom(16).hex()
         self.set(token, session_data)
         return token
 
-    def verify_token(self, token):
+    def verify_token(self, token: str) -> Union[bool, dict]:
         session_data = self.get(token)
         if session_data is not None:
             self.execute(TOUCH_SESSION_SQL, (token,))
@@ -130,11 +130,11 @@ class SessionStore:
     # User account methods
     #
 
-    def add_user(self, username, password):
+    def add_user(self, username: str, password: str):
         hashed, salt = self.hash_password(password)
         self.execute(ADD_USER_SQL, (username, hashed, salt))
 
-    def verify_user(self, username, password):
+    def verify_user(self, username: str, password: str) -> Optional[dict]:
         row = self.execute(LOOKUP_USER_SQL, (username,))
         if row is not None:
             username, stored_hash, salt = row
@@ -143,7 +143,8 @@ class SessionStore:
                 return {"username": username}
         return None
 
-    def hash_password(self, password, salt=None):
+    def hash_password(self, password: str,
+                      salt: Optional[bytes] = None) -> Tuple[str, str]:
         if salt is None:
             salt = os.urandom(16)
         hashed = hashlib.pbkdf2_hmac("sha256", password.encode("utf8"), salt,
